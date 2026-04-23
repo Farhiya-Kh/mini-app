6078cc1add1b0801628ad41fe66fec35695d6ce7
Anisa Hashi <anisahashi@infra04-wg012.eduroam.wireless.umn.edu>
2026-04-23 15:17:51 -0500

    task2 updates

--- app/src/main/java/com/example/myapplication/GameEngine.java
+++ app/src/main/java/com/example/myapplication/GameEngine.java
@@ -1,4 +1,41 @@
 package com.example.myapplication;
 
+import android.app.GameState;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
 public class GameEngine {
+
+    //private GameState currentState = GameState.CREATOR;
+
+    private Handler handler = new MyHandler();
+    private final int FRAME_DELAY = 16; // ~60 FPS
+
+    private boolean isRunning = false;
+    private float birdY;
+    private float velocity;
+    private float gravity = 1.2f;
+    private Runnable onUpdate;
+
+    public GameEngine(Runnable onUpdate) {
+        this.onUpdate = onUpdate;
+    }
+
+    private static class MyHandler extends Handler {
+        @Override
+        public void close() throws SecurityException {
+
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void publish(LogRecord record) {
+
+        }
+    }
 }
